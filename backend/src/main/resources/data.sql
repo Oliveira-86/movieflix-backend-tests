90f75d0012ec41dd72c9e26c94638b64d533ec9a
Egberto Oliveira <egberto.duarte.14@gmail.com>
2021-03-31 17:45:52 -0300

    Added Entities Genre, Movie, and seeding table

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_genre (name) VALUES (action)
+INSERT INTO tb_genre (name) VALUES (horror)
+INSERT INTO tb_genre (name) VALUES (sci-fi)
+
+INSERT INTO tb_movie (title, subTitle, year, imgUrl, synopsis) VALUES (Lord of the Rings, The Fellowship of the Ring, 2001, 
